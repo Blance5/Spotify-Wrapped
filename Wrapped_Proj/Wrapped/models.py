465d1b9c351be52bb075fd54924fa907f0692c46
b-alford <bpalford05@gmail.com>
2024-11-30 16:34:38 -0500

    Update models.py

--- Wrapped_Proj/Wrapped/models.py
+++ Wrapped_Proj/Wrapped/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.db import transaction
+from django.contrib.auth.models import User
+
+class Wrap(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=255)
+    timeframe = models.CharField(max_length=50)
+    generated_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.display_name
 
 class WrapCounter(models.Model):
     current_wrap_id = models.PositiveIntegerField(default=1)
